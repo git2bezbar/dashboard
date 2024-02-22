24e55e4d722b45bfe6305374a737311cc59bada1
Adem <ademduran6888@gmail.com>
2024-02-22 15:10:06 +0100

    feat(app): settings the home page basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import PageHeading from "@/src/components/PageHeading";
+import Subtitle from "@/src/components/Subtitle";
+import Title from "@/src/components/Title";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Template
-    </main>
+    <>
+      <PageHeading>
+        <Title>Rendons votre site web incroyable ! ✨</Title>
+        <Subtitle>Cras elementum mi a libero sagittis sollicitudin. Quisque eleifend dapibus justo, sed euismod ipsum hendrerit non. Donec mollis semper elit.</Subtitle>
+      </PageHeading>
+      <div>
+        <div>
+          <Title tag="h2">Votre charte graphique actuelle 🎨</Title>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </> 
   )
 }
