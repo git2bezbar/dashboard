17d12396352008430fbb4089f56368e6960746ab
Adem <ademduran6888@gmail.com>
2024-03-11 17:28:04 +0100

    fix'sidebar): using the correct prop for child links

--- src/components/Sidebar/SidebarGroup.tsx
+++ src/components/Sidebar/SidebarGroup.tsx
@@ -34,7 +34,7 @@ export default function SidebarGroup({
           <ul className="flex flex-col gap-4">
             { items.map((item, i) => (          
               <li key={i} className={`flex gap-4 p-4 rounded-ui duration-300 bg-white hover:bg-whiteDark text-black font-bold ${ item.isCurrentPage ? "!bg-whiteDark" : ""}`}>
-                <Link className="w-full" href={link}>
+                <Link className="w-full" href={item.link}>
                   <span className={`w-[24px] h-[24px] ${ isCurrentPage ? "bg-primary" : "bg-white"}`}></span>
                   { item.text }
                 </Link>
