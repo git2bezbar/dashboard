c36d5d3a53c5501270c90d61c70643d86bde82d5
Adem <ademduran6888@gmail.com>
2024-05-14 11:10:44 +0200

    fix(components): moving ui components to the src folder

--- src/ui/form.tsx
+++ src/ui/form.tsx
@@ -10,8 +10,8 @@ import {
   useFormContext,
 } from "react-hook-form"
 
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import { cn } from "@/services/utils"
+import { Label } from "@/src/ui/label"
 
 const Form = FormProvider
 
--- src/ui/toaster.tsx
+++ src/ui/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/src/ui/toast"
+import { useToast } from "@/src/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
