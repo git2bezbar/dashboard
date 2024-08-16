ff7c9a50430d9b83e0c95e15e7cd1175cde59edc
Adem <ademduran6888@gmail.com>
2024-08-16 19:31:15 +0200

    fix(components): updating sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,8 +2,9 @@
 
 import Link from "next/link";
 import SidebarItem from "./SidebarItem";
-import SidebarGroup from "./SidebarGroup";
 import { UUID } from "@/services/types";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export interface SidebarProps {
   websiteId: UUID;
@@ -11,14 +12,17 @@ export interface SidebarProps {
 
 export default function Sidebar({ websiteId }: SidebarProps) {
   
-  let pathname = "";
+  const pathname = usePathname();
 
-  if (typeof window !== "undefined")
-    pathname = window.location.pathname;
   return (
     <header className="sticky top-0 bg-black text-white p-8 h-screen flex flex-col gap-16 items-start">
       <Link href={`/${websiteId}`}>
-        <img src="/logo-white.svg"/>
+        <Image
+          src="/logo-white.svg"
+          alt="Logo"
+          width={100}
+          height={100}
+        />
       </Link>
       <nav className="w-full">
         <ul className="flex flex-col gap-4">
@@ -46,17 +50,18 @@ export default function Sidebar({ websiteId }: SidebarProps) {
           >
             Personnalisation
           </SidebarItem>
-          <SidebarGroup
-            groupTitle="Paramètres"
+          <SidebarItem
             link={`/${websiteId}/settings/general`}
-            isCurrentPage={pathname.includes("/settings")}
-            items={
-              [
-                { text: "Généraux", link: `/${websiteId}/settings/general`, isCurrentPage: pathname.includes("/settings/general") },
-                { text: "Contact", link: `/${websiteId}/settings/contact`, isCurrentPage: pathname.includes("/settings/contact") }
-              ]
-            }
-          />
+            isCurrentPage={pathname.includes("/settings/general")}
+          >
+            Paramètres généraux
+          </SidebarItem>
+          <SidebarItem
+            link={`/${websiteId}/settings/contact`}
+            isCurrentPage={pathname.includes("/settings/contact")}
+          >
+            Paramètres de contact
+          </SidebarItem>
         </ul>
       </nav>
     </header>
